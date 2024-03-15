0ec90cbc1ec04c2c1c4ed0fb75fa38175ff5dbdf
hrvieira <luizhrvieira@outlook.com>
2024-03-15 14:25:10 -0300

    Primeiro commit da minha página Links Gen

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var lista = document.getElementById('links');
+
+  var itens = lista.getElementsByClassName('itemLista');
+
+  for (var i = 0; i < itens.length; i++) {
+    itens[i].addEventListener('click', function() {
+      var url = this.getAttribute('data-url');
+
+      window.location.href = url;
+    });
+  }
+});
+
+
+
